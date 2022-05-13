185d8b61cf0e25e412f3d48230f0809d4046cbc7
Vinay King <1780405714@qq.com>
2022-05-13 19:30:54 +0800

    add graph with weighted egde

---
+++ problems/Graphs/Edge.java
@@ -0,0 +1,38 @@
+package algorithm.problems.Graphs;
+
+/**
+ * Edge
+ * an undirective graph which include weighted edges
+ * this is the container edge of weights
+ */
+public class Edge implements Comparable<Edge>{
+    private int v;
+    private int w;
+    private double weight;
+
+    public Edge(int v,int w,double weight){
+
+    }
+
+    public double weight(){
+
+    }
+
+    public int either(){
+        //get one of the end of the edge
+
+    }
+
+    public int other(int v){
+
+    }
+
+    public int compareTo(Edge that){
+
+    }
+
+    public String toString(){
+
+    }
+
+}
